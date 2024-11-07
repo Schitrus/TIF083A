94d66bc8fecb603ead034fd9a31a438697df5bc3
Kalle Andersson <Schitrus@gmail.com>
2024-11-07 14:39:23 +0100

    Removed usage of old parser in animation overview

--- animate_experiment.py
+++ animate_experiment.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-from data_parser import read_data
 from parse_script import parse_marker_data
 
 # Precalculated moment of inertias
@@ -10,7 +9,7 @@ moment_of_inertia = {"5.9": 3.084225e-6, "14.6": 4.5625e-6, "27.7": 8.65625e-6, 
                      "34.3": 6.83e-6, "22.9": 0.0000152542625, "82.2": 0.000038233275, "82.4": 0.0000383263, \
                         "28.3": 0.0000092010375, "43.5": 0.00008632575, "43.3": 0.0000845703125, "396.1": 0.0003083054}
 
-def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start=0.0, end=1.0):
+def generate_animation(filepath, speed=1.0, traceback_time=1.0, start=0.0, end=1.0):
     """Generates animation from the data of the experiment
     
     Arguments:
@@ -22,7 +21,6 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
     end - The end time of the animation from the data as percentage, must be greater than start and no greater than 1.0
     """
     # Read data
-    obj_names, ts, objs = read_data(filepath)
     markers_dict, ts, masses = parse_marker_data(filepath)
     filename = filepath.split('/')[-1].split('.')[0]
 
@@ -55,44 +53,32 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
     point = np.array([0.45,0.45,0])
 
     # Determines the scale of the figure
-    x_min = np.min([np.min(obj[0]) for obj in objs])
-    x_max = np.max([np.max(obj[0]) for obj in objs])
-    y_min = np.min([np.min(obj[1]) for obj in objs])
-    y_max = np.max([np.max(obj[1]) for obj in objs])
+    x_min = np.min([np.min(pos.T[0]) for pos in markers_dict.values()])
+    x_max = np.max([np.max(pos.T[0]) for pos in markers_dict.values()])
+    y_min = np.min([np.min(pos.T[1]) for pos in markers_dict.values()])
+    y_max = np.max([np.max(pos.T[1]) for pos in markers_dict.values()])
 
     # Predetermined unique colors
     colors = ['cornflowerblue', 'crimson', 'green', 'coral', 'darkorchid', 
               'hotpink', 'lightseagreen', 'sandybrown', 'springgreen', 'violet',
               'tomato', 'darkolivegreen', 'darkslateblue', 'darkslategrey', 'lightcoral']
-    
-    positions = []
-    paths = []
-    xpositions = []
-    ypositions = []
-    xpaths = []
-    ypaths = []
 
-    internal_rmms = []
-    external_rmms = []
-    rmms = []
+    rmm = True if len(markers_dict) == 3*len(masses) else False
     total_rmm = gx.plot(0, 0, alpha=0.5, label=f'Total angular momentum', linewidth=3.0, color='DarkSlateGrey')[0]
             
 
+    positions = {name: ax.plot(1, 0, linestyle='', marker='o', alpha=0.6, color=colors[i], markersize=6.0, label=mass)[0] for i, (name, mass) in enumerate(masses.items())}
+    paths = {name: ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=2.0)[0] for i, (name, _) in enumerate(masses.items())}
     if rmm:
         # Track positions and paths for the objects and angular momentum markers
-        positions = [ax.plot(1, 0, linestyle='', marker='o', alpha=0.6, color=colors[i], markersize=6.0, label=obj_names[i*3])[0] for i in range(len(objs)//3)]
-        paths = [ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=2.0)[0] for i in range(len(objs)//3)]
-        xpositions = [ax.plot(1, 0, linestyle='', marker='o', alpha=0.5, color=colors[i], markersize=3.0)[0] for i in range(len(objs)//3)]
-        xpaths = [ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=1.0)[0] for i in range(len(objs)//3)]
-        ypositions = [ax.plot(1, 0, linestyle='', marker='o', alpha=0.5, color=colors[i], markersize=3.0)[0] for i in range(len(objs)//3)]
-        ypaths = [ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=1.0)[0] for i in range(len(objs)//3)]
+        xpositions = {name: ax.plot(1, 0, linestyle='', marker='o', alpha=0.5, color=colors[i], markersize=3.0)[0] for i, (name, _) in enumerate(masses.items())}
+        xpaths = {name: ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=1.0)[0] for i, (name, _) in enumerate(masses.items())}
+        ypositions = {name: ax.plot(1, 0, linestyle='', marker='o', alpha=0.5, color=colors[i], markersize=3.0)[0] for i, (name, _) in enumerate(masses.items())}
+        ypaths = {name: ax.plot(1, 0, linestyle='-', alpha=0.5, color=colors[i], linewidth=1.0)[0] for i, (name, _) in enumerate(masses.items())}
 
-        internal_rmms = [gx.plot(0, 0, alpha=0.75, linewidth=2.0, linestyle=':', color=colors[i])[0] for i in range(len(objs)//3)]
-        external_rmms = [gx.plot(0, 0, alpha=0.75, linewidth=2.0, linestyle='--', color=colors[i])[0] for i in range(len(objs)//3)]
-        rmms = [gx.plot(0, 0, alpha=0.5, linewidth=2.0, color=colors[i])[0] for i in range(len(objs)//3)]
-    else:
-        positions = [ax.plot(1, 0, linestyle='', marker='o', alpha=0.6, color=colors[i], markersize=6.0, label=obj_names[i])[0] for i in range(len(objs))]
-        paths = [ax.plot(1, 0, linestyle='-', alpha=0.6, color=colors[i], linewidth=2.0)[0] for i in range(len(objs))]
+        internal_rmms = {name: gx.plot(0, 0, alpha=0.75, linewidth=2.0, linestyle=':', color=colors[i])[0] for i, (name, _) in enumerate(masses.items())}
+        external_rmms = {name: gx.plot(0, 0, alpha=0.75, linewidth=2.0, linestyle='--', color=colors[i])[0] for i, (name, _) in enumerate(masses.items())}
+        rmms = {name: gx.plot(0, 0, alpha=0.5, linewidth=2.0, color=colors[i])[0] for i, (name, _) in enumerate(masses.items())}
 
     point_line = ax.plot(point[0]*1000, point[1]*1000, linestyle='', marker='x', alpha=0.6, color='black', markersize=3.0, label='Fixed point')[0]
 
@@ -103,8 +89,8 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
     rmm_min = 0
     rmm_max = 0
 
-    internal_rmm = []
-    external_rmm = []
+    internal_rmm = {}
+    external_rmm = {}
     # Initialize total angularmomentum, both internal and external
     total_angularmomentum = np.zeros(len(ts0))
     total_external_angularmomentum = np.zeros(len(ts0))
@@ -136,14 +122,14 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
         moi =  disc_moi # + mass*np.linalg.norm(rs)**2
         
         # Internal and external angular momentum
-        internal_rmm.append(theta*moi*frequency)
-        external_rmm.append(np.cross(rs[1:], mass*vs).T[2])
+        internal_rmm[name] = theta*moi*frequency
+        external_rmm[name] = np.cross(rs[1:], mass*vs).T[2]
         
-        total_angularmomentum += internal_rmm[-1]
-        total_external_angularmomentum += external_rmm[-1]
+        total_angularmomentum += internal_rmm[name]
+        total_external_angularmomentum += external_rmm[name]
 
-        rmm_min = np.min([rmm_min, *internal_rmm[-1], *external_rmm[-1], *(internal_rmm[-1] + external_rmm[-1]), *(total_angularmomentum + total_external_angularmomentum)])
-        rmm_max = np.max([rmm_max, *internal_rmm[-1], *external_rmm[-1], *(internal_rmm[-1] + external_rmm[-1]), *(total_angularmomentum + total_external_angularmomentum)])
+        rmm_min = np.min([rmm_min, *internal_rmm[name], *external_rmm[name], *(internal_rmm[name] + external_rmm[name]), *(total_angularmomentum + total_external_angularmomentum)])
+        rmm_max = np.max([rmm_max, *internal_rmm[name], *external_rmm[name], *(internal_rmm[name] + external_rmm[name]), *(total_angularmomentum + total_external_angularmomentum)])
         
 
     # Go through each frame
@@ -153,33 +139,36 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
         title = ax.set_title(f'{filename} | {ts[step]-ts[0]:.3f} s')
 
         # Loop through all objects and append positions and paths to graphical line
-        if not rmm:
-            for obj, pos, path in zip(objs, positions, paths):
-                pos.set_data([obj[0][step]], [obj[1][step]])
-                path.set_data(obj[0][traceback:step], obj[1][traceback:step]) # Path is traced
-            return *positions, *paths, point_line, title,
-        else:
-            # Objects and angular tracks
-            for obj, xobj, yobj, pos, xpos, ypos, path, xpath, ypath in zip(objs[::3], objs[1::3], objs[2::3], positions, xpositions, ypositions, paths, xpaths, ypaths):
-                pos.set_data([obj[0][step]], [obj[1][step]])
-                path.set_data(obj[0][traceback:step], obj[1][traceback:step])
-                xpos.set_data([xobj[0][step]], [xobj[1][step]])
-                ypos.set_data([yobj[0][step]], [yobj[1][step]])
+        for name, _ in masses.items():
+            cm = markers_dict[f"{name}cm"].T
+            positions[name].set_data([cm[0][step]], [cm[1][step]])
+            paths[name].set_data([cm[0][traceback:step]], [cm[1][traceback:step]])  # Path is traced
+            if rmm:
+                # Angular tracks
+                rotx = markers_dict[f"{name[0]}x"].T
+                roty = markers_dict[f"{name[0]}y"].T
+                xpositions[name].set_data([rotx[0][step]], [rotx[1][step]])
+                ypositions[name].set_data([roty[0][step]], [roty[1][step]])
                 # Trace the angular tracks relative to the objects position
                 # Subtract the objects path from the angular track and add objects current position
-                xpath.set_data(obj[0][step] + np.array(xobj[0][traceback:step]) - np.array(obj[0][traceback:step]), obj[1][step] + np.array(xobj[1][traceback:step]) - np.array(obj[1][traceback:step]))
-                ypath.set_data(obj[0][step] + np.array(yobj[0][traceback:step]) - np.array(obj[0][traceback:step]), obj[1][step] + np.array(yobj[1][traceback:step]) - np.array(obj[1][traceback:step]))
-            
-            for i_rmm, e_rmm, i_rmms, e_rmms, sum_rmms in zip(internal_rmm, external_rmm, internal_rmms, external_rmms, rmms):
-                i_rmms.set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], i_rmm[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
-                e_rmms.set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], e_rmm[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
-                sum_rmms.set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], i_rmm[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize] + e_rmm[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
-                
-            total_rmm.set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], total_angularmomentum[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize] + total_external_angularmomentum[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
+                xpaths[name].set_data(cm[0][step] + np.array(rotx[0][traceback:step]) - np.array(cm[0][traceback:step]), \
+                                      cm[1][step] + np.array(rotx[1][traceback:step]) - np.array(cm[1][traceback:step]))  
+                ypaths[name].set_data(cm[0][step] + np.array(roty[0][traceback:step]) - np.array(cm[0][traceback:step]), \
+                                      cm[1][step] + np.array(roty[1][traceback:step]) - np.array(cm[1][traceback:step]))        
+                internal_rmms[name].set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], \
+                                            internal_rmm[name][int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
+                external_rmms[name].set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], \
+                                             external_rmm[name][int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
+                rmms[name].set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], \
+                                    internal_rmm[name][int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize] \
+                                        + external_rmm[name][int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
 
-            return *positions, *xpositions, *ypositions, *paths, *xpaths, *ypaths, *internal_rmms, *external_rmms, *rmms, total_rmm, point_line, title, 
-
-    fig.legend()
+        if rmm:
+            total_rmm.set_data(ts0[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize], \
+                               total_angularmomentum[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize] + total_external_angularmomentum[int(frame_count*start)//sample_stepsize:int(stepsize*num + frame_count*start)//sample_stepsize])
+            return *positions.values(), *xpositions.values(), *ypositions.values(), *paths.values(), *xpaths.values(), *ypaths.values(), *internal_rmms.values(), *external_rmms.values(), *rmms.values(), total_rmm, point_line, title, 
+        else:
+            return *positions.values(), *paths.values(), point_line, title
 
     ax.set_aspect('equal', 'box')
     marginal = 50
@@ -188,6 +177,7 @@ def generate_animation(filepath, rmm=False, speed=1.0, traceback_time=1.0, start
     gx.set(xlim=(ts0[0], ts0[-1]), xlabel='Time (s)')
     gx.set(ylim=(rmm_min, rmm_max))
 
+    fig.legend()
     # Generate animation with set number of frames and interval between frames (in ms)
     ani = animation.FuncAnimation(fig, update_frame, int((end-start) * frame_count/stepsize), interval=1000*stepsize/actual_speed, blit=True)
     return ani
